98cfe886eb851cdda845776aecc5e25e8032d2f1
0xAliya <penpenla@outlook.com>
2024-02-17 12:57:29 +0800

    feat: update db

--- app/mock/db.ts
+++ app/mock/db.ts
@@ -1,19 +1,16 @@
 import { PlaneCells } from "@/components/playground/type";
 import { RoomDetail } from "../rooms/types";
-import fse from "fs-extra";
 
 type RoomDbDetail = Omit<RoomDetail, "planes"> & {
   allPlacedPlanes: Record<string, PlaneCells[]>;
 };
 
+let db: RoomDbDetail[] = [];
+
 export const readDB = async () => {
-  if (!fse.existsSync("db.json")) {
-    await fse.outputJSON("db.json", []);
-  }
-  const content = await fse.readJSON("db.json");
-  return content as RoomDbDetail[];
+  return db;
 };
 
 export const writeDB = async (data: RoomDbDetail[]) => {
-  await fse.outputJSON("db.json", data);
+  db = data;
 };
